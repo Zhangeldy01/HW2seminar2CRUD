2c740d74ef52cf95042899b03716f7d43184cd43
zhangeldy01 <shakhanov_zhan@mail.ru>
2024-02-28 22:03:07 +0300

    Initial commit

---
+++ src/main/java/com/example/HW2seminar2CRUD/services/UserService.java
@@ -0,0 +1,35 @@
+package com.example.HW2seminar2CRUD.services;
+
+import com.example.HW2seminar2CRUD.models.User;
+import com.example.HW2seminar2CRUD.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+import java.security.PublicKey;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteById(int id) {
+        userRepository.deleteById(id);
+    }
+    public User getOne(int id) {
+        System.out.println("User service get id: " + id);
+        return userRepository.getOne(id);
+    }
+
+    public User updateUser(User user) {
+        return userRepository.update(user);
+    }
+}
